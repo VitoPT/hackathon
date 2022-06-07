da0615cbd21775de0c9d6721df3622707a00f289
FilipeG0 <silvexsilvexsilvex@gmail.com>
2022-06-07 15:21:43 +0100

    Filipe update

---
+++ hackathon/pages/brochure/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Image from 'next/image'
+  
+export default class NextJsCarousel extends Component {
+    render() {
+        return (
+            <div>
+              <h2>Catalog</h2>
+              <Carousel>
+                  <div>
+                    <Image
+                     src="/1.jpeg" 
+                     alt="Imagem1" 
+                     height={696} 
+                     width={414} />
+                      
+  
+                  </div>
+                  <div>
+                  <Image
+                     src="/2.jpeg" 
+                     alt="Imagem2" 
+                     height={696} 
+                     width={414} />
+                
+  
+                  </div>
+                  <div>
+                  <Image
+                     src="/3.jpeg" 
+                     alt="Imagem3" 
+                     height={696} 
+                     width={414} />
+                     
+  
+                  </div>
+                  <div>
+                  <Image
+                     src="/4.jpeg" 
+                     alt="Imagem4" 
+                     height={696} 
+                     width={414} />
+                      
+  
+                  </div>
+                  <div>
+                  <Image
+                     src="/4.jpeg" 
+                     alt="Imagem4" 
+                     height={696} 
+                     width={414} />
+                     
+  
+                  </div>
+              </Carousel>
+            </div>
+        );
+    }
+};
